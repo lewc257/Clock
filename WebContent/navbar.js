35b68b35abf048ac56784ea50f757771c209e134
Lewis <djl700@live.co.uk>
2020-01-03 15:08:43 +0000

    If the user clicks a nav link that leads to the same page, it wont refresh

--- WebContent/navbar.js
+++ WebContent/navbar.js
@@ -6,6 +6,12 @@ var navBarButtons = document.getElementById("nav-bar");
 
 var page = currentLocation[len - 1];
 
+/*
+var stopWatchLink = navBarButtons.children[2];
+var coundDownLink = navBarButtons.children[1];
+var localClockLink = novBarButtons.children[0];
+*/
+
 switch (page){
 	case "countdown.html":
 		navBarButtons.children[1].style.backgroundColor = 'yellow';
@@ -16,4 +22,22 @@ switch (page){
 	case "stopwatch.html": 
 		navBarButtons.children[2].style.backgroundColor = 'yellow';
 		break;
+}
+
+navBarButtons.children[1].onclick = function(e){
+	if (page == "countdown.html"){
+		e.preventDefault();
+	}
+}
+
+navBarButtons.children[0].onclick = function(e){
+	if (page == "local_clock.html"){
+		e.preventDefault();
+	}
+}
+
+navBarButtons.children[2].onclick = function(e){
+	if (page == "stopwatch.html"){
+		e.preventDefault();
+	}
 }
